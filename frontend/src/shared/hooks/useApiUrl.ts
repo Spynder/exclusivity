6e2b661036b323de2f257462ce55fc147786b52e
Spynder <19329095+Spynder@users.noreply.github.com>
2025-05-10 04:51:56 +0300

    trying to fix back

--- frontend/src/shared/hooks/useApiUrl.ts
+++ frontend/src/shared/hooks/useApiUrl.ts
@@ -4,6 +4,6 @@ export function useApiUrl() {
 	if (typeof window === "undefined") return;
 	//if(typeof document.window === "undefined") return "";
 	let url = new URL(`${window.location.origin}`);
-	url.port = '8000';
+	url.port = '';
 	return url.toString().slice(0, -1);
 }
